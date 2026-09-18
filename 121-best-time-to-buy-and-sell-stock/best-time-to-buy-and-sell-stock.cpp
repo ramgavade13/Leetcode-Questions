c9f385f65fb5736950660032091f3e0abcbcf427
Ram Vithoba Gavade <ramgavade4@gmail.com>
2026-09-18 11:47:10 +0530

    Time: 0 ms (100.00%) | Memory: 97.4 MB (26.32%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int left =0;
+        int right = 1;
+        int maxprofit =0;
+
+        while( right < prices.size()){
+            if(prices[left] < prices[right]) maxprofit = max(maxprofit , prices[right] - prices[left]);
+
+            else {
+                left = right ;
+            }
+            right ++;
+        }
+        return maxprofit ;
+    }
+};
