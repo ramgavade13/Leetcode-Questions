705b8b6e709c3750a213e60af5ebfe9097b85546
Ram Vithoba Gavade <ramgavade4@gmail.com>
2026-09-20 08:06:56 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (78.39%) - LeetSync

---
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int ans =0;
+
+        for (int i=0;i< s.size() ;i++){
+            int r = 'z' - s[i] + 1;
+            ans += r * (i+1);
+        }
+        return ans ;
+        
+    }
+};
